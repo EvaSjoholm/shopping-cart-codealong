3839e906cbd7f3981626104db30efc21d5ae1579
Eva Sjöholm <e.sjoholm.int@gmail.com>
2023-04-17 19:25:52 +0200

    trying to fix ESLINT

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,3 +1,4 @@
+
 import { createSlice } from '@reduxjs/toolkit'
 
 export const cart = createSlice({
@@ -5,5 +6,16 @@ export const cart = createSlice({
   initialState: {
     items: []
   },
-  reducers: {}
+  reducers: {
+    addItem: (state, action) => {
+        const existingProduct = state.items.find((item) => item.id === action.payload.id)
+
+        if (existingProduct) {
+        existingProduct.quantity += 1
+        } else {
+
+      state.items.push({ ...action.payload, quantity: 1 }) 
+    }
+  }
+}
 })
